4271cc6d5d7bbe7b88cf1fefed2c9e775c5dc6a0
MohamedElHassan <mohammedelhassan2001@gmail.com>
2024-06-21 14:13:10 +0300

    fixed middleware.ts issue

--- middleware.ts
+++ middleware.ts
@@ -1,46 +1,8 @@
-// import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// // Make sure that the `/api/webhooks/(.*)` route is not protected here
-// export default clerkMiddleware({
-//   ignoredRoutes: ["/api/webhooks(.*)"],
-// })
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
-
-// import { authMiddleware } from "@clerk/nextjs/server";
-
-// export default authMiddleware({
-//   ignoredRoutes: ["/api/webhooks(.*)"],
-// });
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
-
-
-
-// import { clerkMiddleware } from '@clerk/nextjs/server';
-
-// // Make sure that the `/api/webhooks/(.*)` route is not protected here
-// export default clerkMiddleware();
-
-// export const config = {
-//   matcher: [
-//     '/((?!.*\\..*|_next|api/webhooks).*)', // Exclude api/webhooks
-//     '/',
-//     '/(api|trpc)(.*)',
-//   ],
-// };
-
-
-import { authMiddleware } from "@clerk/nextjs/server";
-
-export default authMiddleware({
-  ignoredRoutes: ["/api/webhooks(.*)"],
-});
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)','/api/webhooks/(.*)'],
 };
